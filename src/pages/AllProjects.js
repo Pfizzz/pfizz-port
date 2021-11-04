cc335a65c41e2c2026fcd573375bc8088f6a6e29
Pfizz <pfiste80@gmail.com>
2021-11-03 21:56:53 -0400

    fixed data, links

--- src/pages/AllProjects.js
+++ src/pages/AllProjects.js
@@ -8,7 +8,8 @@ const DUMMY_DATA = [
         'https://github.com/Pfizzz/weather2/blob/887bb80d1064efe1ab5cf8bb1c85da87de737bc9/assets/images/ss1.png?raw=true',
       address: 'An application using the One Call weather API made using JavaScript',
       description:
-        'This project rocks',
+      <a href='https://github.com/Pfizzz/weather2' target='_blank' rel="noreferrer">Github</a>,
+      link: <a href='https://pfizzz.github.io/weather2/' target='_blank' rel="noreferrer">Live Project</a>
     },
     {
       id: 'm2',
@@ -17,8 +18,20 @@ const DUMMY_DATA = [
         'https://github.com/Pfizzz/code-quiz-final/blob/02d24b95d1c76004931a0cb614642f021281fd6e/assets/images/ss1.png?raw=true',
       address: 'A coding quiz built with vanilla JavaScript',
       description:
-        'This project is okay',
+      <a href='https://github.com/Pfizzz/code-quiz-final' target='_blank' rel="noreferrer">Github</a>,
+      link: <a href='https://pfizzz.github.io/code-quiz-final/' target='_blank' rel="noreferrer">Live Project</a>
     },
+    {
+      id: 'm3',
+      title: '&Chill',
+      image:
+        'https://raw.githubusercontent.com/Pfizzz/project-1/fbb2eea178e5b8f86f18b59d77f4855f38feef74/assets/images/logo.png?raw=true',
+      address: 'A Javascript/Materialize CSS project',
+      description:
+        <a href='https://github.com/Pfizzz/project-1' target='_blank' rel="noreferrer">Github</a>,
+        link: <a href='https://pfizzz.github.io/project-1/' target='_blank' rel="noreferrer">Live Project</a>
+    }
+    
   ];
 
 function PortfolioPage() {
