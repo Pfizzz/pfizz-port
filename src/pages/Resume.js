046429cce943ab88b18e0c25e26d7949f0a7734a
Pfizz <pfiste80@gmail.com>
2021-11-07 20:49:21 -0500

    resume is responsive

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,8 +1,9 @@
 import resume from '../images/resume.png'
+import classes from '../components/layout/MainNavigation.module.css';
 
 function ResumePage() {
     return <div>
-        <img src={resume}/>
+        <img src={resume} className={classes.logo1}/>
     </div>
 }
 
